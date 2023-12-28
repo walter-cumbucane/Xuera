d802e13f788b86e8e519807ef40e85657d62ce64
walter-cumbucane <cumbucane.walter@gmail.com>
2023-12-29 00:29:39 +0200

    POST. LET'S STORE INTO DATABASES

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-
-@app.route("/ivanilson")
-def first():
+@app.route("/")
+def main():
     return render_template("index.html")
+
+
+@app.route("/ivanilson", methods=["post"])
+def first():
+    name = request.form.get("name")
+    password = request.form.get("password")
+    email = request.form.get("email")
+    return render_template("success.html", name=name)
 
 
 if __name__ == "__main__":
